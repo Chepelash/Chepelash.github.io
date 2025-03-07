05ef9bf9795a0feb5f7b005f6a8f443c00eb232b
Anton Chepelev <chepelash@gmail.com>
2025-03-07 22:00:10 +0300

    feat: add personal info

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import gb_cert from "./assets/gb_cert.png";
+import otus_cert from "./assets/otus_cert.png";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,20 +15,24 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Знакомство</h1>
+      <h4>Меня зовут Антон Чепелев</h4>
+      <div className="polaroid">
+        <img src={gb_cert} className="cert_img" />
+        <p>В 2017 году проходил курсы Python в Geekbrains</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="polaroid">
+        <img src={otus_cert} className="cert_img" />
+        <p>В 2024 году прошел курсы Rust в Otus</p>
+      </div>
+      <h4>Цели</h4>
+      <ul>
+        <li>
+          Разобраться в разработке на React для создания GUI для Rust приложений
+        </li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
